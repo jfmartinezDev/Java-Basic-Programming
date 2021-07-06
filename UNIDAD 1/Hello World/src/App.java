60bb426f34f7d32e71c4bbdd56d453006402db85
jfmartinezDev <jfma.8925@gmail.com>
2021-07-06 00:07:50 -0500

    Added third print message

--- UNIDAD 1/Hello World/src/App.java
+++ UNIDAD 1/Hello World/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("This is Basic programming with Java course!")
+        System.out.println("This is Basic programming with Java course!");
+        System.out.println("Let's get started!");
     }
 }
