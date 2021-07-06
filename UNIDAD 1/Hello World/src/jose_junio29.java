26f4f3c763542294f99b478438ebc5cd494f9379
jfmartinezDev <jfma.8925@gmail.com>
2021-07-05 22:13:59 -0500

    Initial commit

---
+++ UNIDAD 1/Hello World/src/jose_junio29.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class jose_junio29 {
+    public static void main(String[] args) throws Exception {
+        // Objetos son tipos de datos que yo mismo creo
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Ingese una palabra");
+        // String entrada = scanner.nextLine();
+        // System.out.println("El mensaje fue "+entrada);
+        // int resultado = scanner.nextInt();
+        // System.out.println("El mensaje fue " + resultado);
+        // if (5>7){
+        //     System.out.println(":)");
+        // }
+        // else if (3>2){
+        //     System.out.println("T.T");
+        // }
+        // else{
+        //     System.out.println(":(");
+        // }
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // if (animal.equals("Perro")){
+        //     System.out.println("Woff");
+        // }
+        // if (animal.equals("Gato")){
+        //     System.out.println("Miau");
+        // }
+        // else {
+        //     System.out.println("No se encontro");
+        // }
+        
+        // switch(animal){
+        //     case "Perro":
+        //        System.out.println("Woff");
+        //        break;
+        //     case "Gato":
+        //        System.out.println("Miau");
+        //        break;
+        //     default:
+        //        System.out.println("No se encontro");
+        
+        // int numero = 5;
+        // while(true){
+        //     contado
+
+        // }
+               
+        // int numero = 5;
+        // do{
+        //     System.out.println(":)");
+        // }
+        // while(numero>7);
+        
+        // for (int i=0; i<5; i=i+1){
+        //     System.out.println(i);
+        // }
+
+        for (int i=7; i>0; i=i-1){
+            System.out.println(i);
+        }
+
+        for (int i=0; i<20; i=i+2){
+            if (i==8){
+                //continue;
+                break;
+            }
+            System.out.println(i);
+        }
+        
+    }
+
+
+}
---
+++ UNIDAD 1/Hello World/src/jose_junio29b.java
@@ -0,0 +1,78 @@
+public class jose_junio29b {
+    public static void main(String[] args){
+        int a = 7;
+
+        // System.out.println(a+5);
+        // System.out.println(a-5);
+        // System.out.println(a*5);
+        // System.out.println(a/5);
+        // System.out.println(a%5);
+        // System.out.println(Math.pow(4,3));
+        // System.out.println(Math.sqrt(16));
+        // System.out.println(Math.abs(3-5));  
+        // System.out.println(Math.cbrt(64));
+
+        // int contador = 0;
+        // contador = contador + 1;
+        // contador += 1;
+        // contador++;
+        // System.out.println(contador);
+
+        // for (int i=7; i<0; i=i--){
+        //     System.out.println(i);
+        // }
+
+        // int numero = 1;
+        // System.out.println(numero++); // Primero muestra el numero y luego lo incrementa
+        // System.out.println(numero);
+
+        // System.out.println(++numero); // Primero mincrementa el numero y luego lo muestra
+        // System.out.println(numero);
+
+        // System.out.println(numero--); 
+        // System.out.println(++numero);
+
+        // char caracter = 'j';
+        // System.out.println(++caracter);
+
+        // char caracter = 'z';
+        // System.out.println(++caracter);
+
+        // char caracter = 'a';
+        // System.out.println(--caracter);
+
+        // String resultado = 5>2 ? ":)" : ":(";
+        // System.out.println(resultado);
+
+        // if (5>3 | 4>6){
+        //     System.out.println(":)");
+        // }
+
+        // if (!(1>3)){
+        //     System.out.println(":)");
+        // }
+
+        // if (5>7 & 8>1 & 7>1 & 9>1){
+        //     System.out.println(":)");
+        // }
+
+        // if (5>7 && 8>1 && 7>1 && 9>1){
+        //     System.out.println(":)");
+        // }
+
+        // if (5>7 | 8>1 | 7>1 | 9>1){
+        //     System.out.println(":)");
+        // }
+
+        // if (5>7 || 8>1 || 7>1 || 9>1){
+        //     System.out.println(":)");
+        // }
+
+        if (5>7 ^ 5>3){
+            System.out.println(":)");
+        }
+
+
+    }
+    
+}
